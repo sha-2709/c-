64cb051895f2b67fc7af3a97ddf7a7380d387d90
Anisha S <sriramanisha63@gmail.com>
2026-07-08 15:32:34 +0530

    Implement sum of two numbers in C++

---
+++ 13.cpp
@@ -0,0 +1,71 @@
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     float p=1000 , r=30 , t=3 ;
+//     float si=p*r*t*0.01;
+//     cout<<"Simple Intrest : ";
+//     cout<<si;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     float p=1000;
+//     float r=30;
+//     float t=3;
+//     float si=(p*r*t)/100;
+//     cout<<"Simple intrest : ";
+//     cout<<si;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     float r=2;
+//     float v=(4*3.141592*r*r*r)/3;
+//     cout<<"Volume of sphere : ";
+//     cout<<v;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x;
+//     cin>>x;
+//     cout<<x*x;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x;
+//     cout<<"Number= ";
+//     cin>>x;
+//     cout<<endl;
+//     cout<<"Square of the number= ";
+//     cout<<x*x;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x;
+//     cout<<"Enter a number: ";
+//     cin>>x;
+//     cout<<endl;
+//     cout<<"Square of this number is: ";
+//     cout<<x*x;
+// }
+
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y;
+    cout<<"Enter two numbers: ";
+    cin>>x;
+    cin>>y;
+    cout<<endl;
+    cout<<"Sum of these two numbers is: ";
+    cout<<x+y;
+}
