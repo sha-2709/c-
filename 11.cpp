c816ddd35c7a1777c6974b44d7d49d4e9269b337
Anisha S <sriramanisha63@gmail.com>
2026-07-08 15:28:37 +0530

    Create 11.cpp

---
+++ 11.cpp
@@ -0,0 +1,49 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x=5, y=2;
+//     cout<<x+y<<endl; //7
+//     cout<<x-y<<endl; //3
+//     cout<<x*y<<endl; //10
+//     cout<<x/y<<endl; //2.5 (problem-it goes to neither nearest nor greatest integer next one is solution )
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     float x=5, y=2;
+//     cout<<x+y<<endl;
+//     cout<<x-y<<endl;
+//     cout<<x*y<<endl;
+//     cout<<x/y<<endl;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     float x=3.14;
+//     float y=2.6;
+//     cout<<x+y<<endl;
+//     cout<<x-y<<endl;
+//     cout<<x*y<<endl;
+//     cout<<x/y<<endl;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x=4;
+//     cout<<x<<endl;
+//     x=x+1;
+//     cout<<x<<endl;
+//     x+=1;
+//     cout<<x<<endl;
+//     x++;
+//     cout<<x<<endl;
+//     ++x;
+//     cout<<x<<endl;
+//     x--;
+//     cout<<x<<endl;
+//     --x;
+//     cout<<x<<endl;
+// }
