f977dfc7b8e06938829d032599194f64742f1181
Anisha S <sriramanisha63@gmail.com>
2026-07-08 15:33:48 +0530

    Add multiple C++ programs for basic arithmetic

---
+++ 14.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"Enter 1st number: ";
+    cin>>x;
+    int y;                  //automatically comes in next line so no need to use cout<<endl;
+    cout<<"Enter 2nd number: ";
+    cin>>y;
+    cout<<"Sum of the two numbers is: ";
+    cout<<x+y;
+}
+
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"Enter 1st number: ";
+    cin>>x;
+    int y;                  
+    cout<<"Enter 2nd number: ";
+    cin>>y;
+    cout<<"Sum of the two numbers is: "<<x+y;
+}
+
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y;
+    cout<<"Enter 1st number: ";
+    cin>>x;                
+    cout<<"Enter 2nd number: ";
+    cin>>y;
+    cout<<"Sum of the two numbers is: "<<x+y;
+}
+
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y,z;
+    cout<<"Enter the 1st number: ";
+    cin>>x;
+    cout<<"Enter the 2nd number: ";
+    cin>>y;
+    cout<<"Enter the 3rd number: ";
+    cin>>z;
+    cout<<"Sum of the three numbers is: "<<x+y+z;
+}
+
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y,z;
+    cout<<"Enter the 1st number: ";
+    cin>>x;
+    cout<<"Enter the 2nd number: ";
+    cin>>y;
+    cout<<"Enter the 3rd number: ";
+    cin>>z;
+    cout<<"Product of the three numbers is: "<<x*y*z;
+}
+
+#include<iostream>
+using namespace std;
+int main(){
+    int x=8;
+    int y=67;
+    cout<<x%y;
+}
+
