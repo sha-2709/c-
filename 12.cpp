0b5fcf097a54020977370848ad7aa027a45c064e
Anisha S <sriramanisha63@gmail.com>
2026-07-08 15:30:50 +0530

    Create 12.cpp

---
+++ 12.cpp
@@ -0,0 +1,62 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x=5;
+//     x++;
+//     cout<<x<<endl;
+//     x--;
+//     cout<<x<<endl;
+//     ++x;
+//     cout<<x<<endl;
+//     --x;
+//     cout<<x<<endl;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x=4;
+//     cout<<x<<endl;
+//     cout<<x++<<endl;  //post increment
+//     cout<<x<<endl;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x=4;
+//     cout<<x<<endl;
+//     cout<<++x<<endl;  //pre increment
+//     cout<<x<<endl;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     float r=4.3;
+//     float a=3.141592*r*r;
+//     cout<<a;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     float r=4.3;
+//     cout<<3.141592*r*r;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     float r=9;
+//     cout<<"Area is:"<<endl;
+//     cout<<3.141592*r*r;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     float r=9;
+//     cout<<"Area is : ";
+//     cout<<3.141592*r*r;
+// }
