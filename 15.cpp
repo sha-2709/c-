9ca1f71c3f46b38a3c79617474ab1677044e5bea
Anisha S <sriramanisha63@gmail.com>
2026-07-08 15:35:02 +0530

    Add multiple C++ examples demonstrating typecasting

---
+++ 15.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cin>>x;
+    float y=(float)x;   //typecasting
+    cout<<y/2;
+}
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    int i=2*3/4;
+    cout<<i;
+}
+
+#include<iostream>
+using namespace std;
+int main(){
+    char ch;
+    cin>>ch;
+    cout<<ch;
+}
+
+#include<iostream>
+using namespace std;
+int main(){
+    char ch;        
+    cin>>ch;
+    cout<<(int)ch;    //ascii value using typecasting
+}
+
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cin>>x;
+    char ch=(char)x;       //reverse of previous process
+    cout<<ch;
+}
+
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<5/2<<endl;
+    cout<<5.0/2<<endl;
+    cout<<5/2.0<<endl;
+    cout<<5.0/2.0<<endl;
+}
